5007f35a0a91f363e69867f86290e2380b760c9e
刁琪 <diaoqi@diaoqi.local>
2020-09-03 14:30:44 +0800

    更新项目名称

--- src/models/user/model.js
+++ src/models/user/model.js
@@ -18,35 +18,6 @@ export default {
       const response = yield call(userApi.getCode, payload)
       callback && callback(response)
     },
-    *register({ payload, callback }, { call, put }) {
-      const response = yield call(userApi.register, payload)
-      callback && callback(response)
-    },
-    *login({ payload, callback }, { call, put }) {
-      const response = yield call(userApi.login, payload)
-      callback && callback(response)
-    },
-    *codeLogin({ payload, callback }, { call, put }) {
-      const response = yield call(userApi.codeLogin, payload)
-      callback && callback(response)
-    },
-    *getUserInfo({ payload, callback }, { call, put }) {
-      const response = yield call(userApi.getUserInfo, payload)
-      callback && callback(response)
-      if (response.code == 200) {
-        sessionStorage.setItem('userInfo', JSON.stringify(response.data))
-        yield put({
-          type: 'save',
-          payload: {
-            userInfo: response.data
-          }
-        })
-      }
-    },
-    *findPassword({ payload, callback }, { call, put }) {
-      const response = yield call(userApi.findPassword, payload)
-      callback && callback(response)
-    },
   },
 
   reducers: {
--- src/packageA/pages/weekList/index.js
+++ src/packageA/pages/weekList/index.js
@@ -187,7 +187,7 @@ export default class WeekList extends Component {
         }
       }]
     };
-  
+    
     chart.setOption(option);
     return chart;
   }
